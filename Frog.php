c2457905f76aa90d63208bb7256efe59ae289ca1
vernandaerika11 <68937378+vernandaerika11@users.noreply.github.com>
2020-07-30 22:14:03 +0700

    Update Frog.php

--- Frog.php
+++ Frog.php
@@ -1,13 +1,13 @@
 <?php
 class Frog extends animal {
 	public $kaki =4 ;
-	public $jump;
+	public $jump ='hop hop';
 	//public jump();
 	public function get_legs(){
 		echo $this->kaki;
 	}
 	public function jump(){
-		echo "hop hop";
+		echo $this->jump;
 	}
 }
 
